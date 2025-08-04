848af0cba8247f4cae17cb6f217609a0bd26cbd7
viswanathpradap <161550369+Viswanathpradap@users.noreply.github.com>
2025-08-04 07:45:06 +0530

    Revert "Viswa"

--- priyaviswa.java
+++ priyaviswa.java
@@ -1,10 +1,7 @@
 package viswapriya;
 
 public class priyaviswa extends Vehicle {
-	void show() {
-		System.out.println("Child method");
-		System.out.println("child method will display");
-		}
+	void show() { System.out.println("Child method"); }
 
 	
 		
