ee2b1c9f6bf8fdbf0119b62edc460c46a1a116ee
viswanathpradap <161550369+Viswanathpradap@users.noreply.github.com>
2025-08-05 23:41:48 +0530

    Update priyaviswa.java

--- priyaviswa.java
+++ priyaviswa.java
@@ -4,11 +4,14 @@ public class priyaviswa extends Vehicle {
 	void show() {
 		System.out.println("Child method");
 		System.out.println("child method will display");
+		System.out.println("child method will not display");
+		
 		}
 
 	
 		
 		
 	}
+
 
 
